a22a87cd2d5e87d8246e407af88de5ca005f9239
Thomas Boles <thomas211738@gmail.com>
2024-05-24 14:06:24 -0400

    connected to backend server

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,25 @@
 import { config } from 'dotenv';
 config();
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
-
 const mongoDBURL = process.env.mongoDBURL;
-const PORT = process.env.PORT_N;
+const PORT = process.env.PORT;
 
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send('Welcome To the KTP App');
   });
 
+mongoose
+.connect(mongoDBURL)
+.then(() => {
+console.log('App connected to database');
 app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`)
+    console.log(`App is listening to port: ${PORT}`);
+});
+})
+.catch((error) => {
+console.log(error);
 });
