35ae1f173b903e3eb57f25b0af3c0a7fa93a470a
Thomas Boles <thomas211738@gmail.com>
2024-06-29 17:44:59 -0400

    fixed firebase warning

--- KTPActivities/app/firebaseConfig.js
+++ KTPActivities/app/firebaseConfig.js
@@ -1,15 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import { 
-FIREBASE_API_KEY, 
-FIREBASE_AUTH_DOMAIN, 
-FIREBASE_PROJECT_ID, 
-FIREBASE_STORAGE_BUCKET, 
-FIREBASE_MESSAGING_SENDER_ID, 
-FIREBASE_APP_ID, 
-FIREBASE_MEASUREMENT_ID 
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID,
 } from '@env';
 
 const firebaseConfig = {
@@ -19,9 +20,13 @@ const firebaseConfig = {
   storageBucket: FIREBASE_STORAGE_BUCKET,
   messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
   appId: FIREBASE_APP_ID,
-  measurementId: FIREBASE_MEASUREMENT_ID
+  measurementId: FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+
+// Initialize Firebase Auth with AsyncStorage persistence
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
